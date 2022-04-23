edd9c6a1129b1e274d20a1017b4e1960543813ef
krisser99 <richardbmt123@gmail.com>
2022-04-23 23:07:30 +0700

    up load warn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Login from './components/Login/Login'
+import './App.scss';
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={
-          <>
-            <Header /> <Home />
-          </>}
-        />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
+      <div className="mobie">
+        <h2>This website don't support mobie</h2>
+        <h2>Please use PC for it</h2>
+      </div>
+      <div className="pc">
+        <Routes>
+          <Route path='/' element={
+            <>
+              <Header /> <Home />
+            </>}
+          />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 
